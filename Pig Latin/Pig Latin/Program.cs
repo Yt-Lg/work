229ef45ad81726dd50d97a634df8f9b4160f5974
YL <67716376+Yt-Lg@users.noreply.github.com>
2021-04-26 00:33:48 +0100

    Add files via upload

---
+++ Pig Latin/Pig Latin/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pig_Latin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+
+            {
+
+                try
+                {
+                    
+                    Console.WriteLine("\n\ninput phrase\n");
+
+                    string phrase = Console.ReadLine();
+                    Console.WriteLine("\n");
+
+                    string[] words = phrase.Split();
+
+
+
+                    foreach (var word in words)
+                    {
+                        Console.Write(word.Substring(1));
+                        Console.Write((word.Substring(0, 1) + "ay "));
+                    }
+
+                }
+
+                catch
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+
+
+
+            }
+        }
+    }
+}
